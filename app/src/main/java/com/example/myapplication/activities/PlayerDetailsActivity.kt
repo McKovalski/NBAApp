6d67daef6884aa9a6b1aed2866904d0588b78ef8
kovacicnino <nk52544@fer.hr>
2022-05-18 22:47:24 +0200

    toolbar changes for detail activities

--- app/src/main/java/com/example/myapplication/activities/PlayerDetailsActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/PlayerDetailsActivity.kt
@@ -21,23 +21,15 @@ class PlayerDetailsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Naziv igraca"
+        binding.arrowBack.setOnClickListener {
+            finish()
+        }
+        //binding.toolbarTitle.text = "Neki title"
 
         val playerDetailsPagerAdapter = PlayerDetailsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
         viewPager.adapter = playerDetailsPagerAdapter
         val tabs: TabLayout = binding.tabLayout
         tabs.setupWithViewPager(viewPager)
-    }
-
-    // overridamo ovu metodu kako bi se vratili na prosli activity u stanju kojem smo ga ostavili
-    // tj. ne pokrecemo ponovno home activity, vec samo gasimo ovaj
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/myapplication/activities/TeamDetailsActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/TeamDetailsActivity.kt
@@ -20,23 +20,14 @@ class TeamDetailsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Naziv tima"
+        binding.arrowBack.setOnClickListener {
+            finish()
+        }
 
         val teamDetailsPagerAdapter = TeamDetailsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
         viewPager.adapter = teamDetailsPagerAdapter
         val tabs: TabLayout = binding.tabLayout
         tabs.setupWithViewPager(viewPager)
-    }
-
-    // overridamo ovu metodu kako bi se vratili na prosli activity u stanju kojem smo ga ostavili
-    // tj. ne pokrecemo ponovno home activity, vec samo gasimo ovaj
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
